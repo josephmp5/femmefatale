4b236747c4abd97cab91b99927df951acb19824f
Yakup Özmavi <jacobmavi@MacBookAir.home>
2024-11-09 00:23:19 +0300

    bugs and issues fixed

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 class Constants {
-  static String uri = "OPENAI_API_KEY";
+  static String uri =
+      "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";
 }
